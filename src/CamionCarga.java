f9e29d6077b46c8ab106ae264548d194bb2e1681
Nicolas <you@example.com>
2026-08-17 22:28:27 -0500

    Agrega clases Automovil y Motocicleta

---
+++ src/CamionCarga.java
@@ -0,0 +1,24 @@
+public class CamionCarga extends Vehiculo{
+    private double capacidadToneladas;
+    public CamionCarga(String placa, String marca, String modelo, int anio, double precioBase, double capacidadToneladas) {
+        super(placa, marca, modelo, anio, precioBase);
+        setCapacidadToneladas(capacidadToneladas);
+    }
+    public double getCapacidadToneladas() {
+        return capacidadToneladas;
+    }
+    public void setCapacidadToneladas(double CapacidadToneladas) {
+        if (capacidadToneladas <= 0){
+            System.out.println("Error: la capacidad debe ser mayor a 0.");
+            return;
+        }
+        this.capacidadToneladas = CapacidadToneladas;
+    }
+
+    public double calcularPrecioFinal() {
+        double recargo = capacidadToneladas * 500000;
+        double precio = getPrecioBase() * 1.05 + recargo;
+
+        return precio;
+    }
+}
